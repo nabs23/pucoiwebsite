c72ba06e52bd904a5b59ac24218bc04ff88ddc4e
Nabel Odal <nabelodal@gmail.com>
2017-11-30 22:35:01 +0800

    Installed Laravel and Minimall Theme

---
+++ resources/views/home.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.simple')
+
+@section('content')
+
+<header class="bg-green">
+  <div class="container text-center">
+    <h1>PHILIPPINE ULAMA CONGRESS<br/> ORGANIZATION, INC.</h1>
+    <p class="text-center">A landing page template freshly redesigned for Bootstrap 4</p>
+  </div>
+</header>
+
+<section id="about" class="bg-light">
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+
+        <h2>About Us</h2>
+        <p class="">
+          The Philippine Ulama Congress Organization, Inc. (PUCOI) is a duly registered religious, non-stock and non- profit organization whose aim is to preserve, propagate and promote the balanced teachings of Islam. 
+        </p>
+        <a href="#about">Read more...</a>
+
+        <hr />
+
+        <h2>History</h2>
+        <p class="">
+          The Organization. The PUCOI was founded and duly registered with the Securities and Exchange Commission on the 13th of December 2005 by the late ‘Aalim Usman Imam Shiek Al-Aman and his group of elder scholars. 
+        </p>
+        <a href="#history">Read more...</a>
+
+        <hr />
+
+        <h2>Vision</h2>
+        <p class="">
+          A proactive and productive organization of Muslim educators that serves as a vicegerent of Islamic faith, instrument of harmonious existence and agent of positive change.
+        </p>
+        <a href="#history">Read more...</a>
+
+        <hr />
+
+        <h2>Mission</h2>
+        <p class="">
+         To serve as an effective instrument in the establishment of a balanced and harmonious society that is built on the pillars of excellence, partnership and service.
+        </p>
+        <a href="#history">Read more...</a>
+
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<section id="contact">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+        <h2>Contact us</h2>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+@endsection
---
+++ resources/views/layouts/minimall.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- Page title -->
+    <title>Philippine Ulama Congress Organization, Inc.</title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Icons -->
+    <link rel="shortcut icon" href="minimall/img/favicon.png">
+    <link rel="apple-touch-icon" href="minimall/img/apple-touch-icon-32x32.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="minimall/img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="minimall/img/apple-touch-icon-114x114.png">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="minimall/css/theme.css">
+
+</head>
+<body data-spy="scroll" data-target=".navbar">
+
+
+<!-- PRELOADER -->
+<div id="preloader">
+    <div class="loader">Loading...</div>
+</div>
+<!-- PRELOADER -->
+
+
+<!-- HEADER -->
+<header id="home" data-stellar-background-ratio="0.5">
+
+    <div class="header-text">
+
+        <h1>Philippine Ulama Congress Organization, Inc.</h1>
+
+        <!-- Navigation -->
+        <nav id="navigation" class="navbar">
+            <button type="button"
+                    class="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#navbar"
+                    aria-expanded="false"
+                    aria-controls="navbar">
+
+                <i class="fa fa-2x fa-list"></i>
+            </button>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a class="underline" href="#home">Home</a></li>
+                    <li><a class="underline" href="#about">About</a></li>
+                    <li><a class="underline" href="#features">Features</a></li>
+                    <li><a class="underline" href="#portfolio">Portfolio</a></li>
+                    <li><a class="underline" href="#testimonials">Testimonials</a></li>
+                    <li><a class="underline" href="#price">Price</a></li>
+                    <li><a class="underline" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav><!-- /Navigation -->
+
+    </div>
+
+</header>
+<!-- END OF HEADER -->
+
+
+@yield('content')
+
+<!-- FOOTER -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+
+            <!-- Social icons -->
+            <div class="col-md-6">
+                <div class="footer-social">
+                    <a href="#" class="btn-colored fa fa-facebook"></a>
+                    <a href="#" class="btn-colored fa fa-google-plus"></a>
+                    <a href="#" class="btn-colored fa fa-twitter"></a>
+                </div>
+            </div><!-- /Social icons -->
+
+            <!-- Footer links and copyright -->
+            <div class="col-md-6 footer-terms">
+                <div>
+                    <a href="#">Information</a>
+                    <a href="#">Terms of service</a>
+                    <a href="#">Private police</a>
+                </div>
+                <div>
+                    Copyright &copy; 2016 Company Name
+                </div>
+            </div><!-- /Footer links and copyright -->
+
+        </div>
+    </div>
+</footer>
+<!-- END OF FOOTER -->
+
+<!-- SCRIPTS -->
+<script src="minimall/js/jquery-2.2.0.min.js"></script>
+<script src="minimall/js/bootstrap.min.js"></script>
+<!--[if lt IE 9]>
+    <script src="minimall/js/html5shiv.min.js"></script>
+    <script src="minimall/js/respond.min.js"></script>
+<![endif]-->
+<script src="minimall/js/jquery.stellar.min.js"></script>
+<script src="minimall/js/isotope.pkgd.min.js"></script>
+<script src="minimall/js/jquery.magnific-popup.min.js"></script>
+<script src="minimall/js/owl.carousel.min.js"></script>
+<script src="minimall/js/jquery.validate.min.js"></script>
+<script src="minimall/js/general.js"></script>
+<!-- END OF SCRIPTS -->
+
+
+</body>
+</html>
